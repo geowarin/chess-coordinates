5d45cbdcf79d296e5af273138767571031a0b0bb
Geoffroy Warin <code@geowarin.com>
2025-07-29 22:41:03 +0200

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import { useState } from "react";
 
+const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
+
 export function App() {
-  const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
-  const ranks = [8, 7, 6, 5, 4, 3, 2, 1];
   const [clickedSquares, setClickedSquares] = useState<Set<string>>(new Set());
 
   const handleSquareClick = (square: string) => {
